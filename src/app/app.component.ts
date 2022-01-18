a25c13e01e28c4dd4700329d8ffcbd02020e95a2
Calyn Greene <calyngreene@rocketmortgage.com>
2022-01-18 09:05:19 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { menuItem } from './menuItem';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'menu';
+  menuItems:menuItem[]=[new menuItem("Blackened Salmon Pizza", "Dinner", 20), new menuItem("Cajun Chicken Alfredo Pizza", "Dinner", 20), new menuItem("Shrimp Pizza", "Dinner", 20), new menuItem("4-Cheese Breadsticks", "Appetizer", 8), new menuItem("WingDings", "Appetizer", 8),new menuItem("Chicken Parmesean Pizza ", "Dinner", 20), new menuItem("Ceaser Salad", "Appetizer", 10)
+  , new menuItem("Top Shelf Margarita", "Cocktails", 10) , new menuItem("Top Shelf Long Island", "Cocktails", 10)];
+ 
+
 }
---
+++ src/app/menuItem.ts
@@ -0,0 +1,16 @@
+export class menuItem{
+    name: string;
+    category: string;
+    price:number;
+
+
+    constructor(name:string, category:string, price:number)
+    {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+
+ 
+}
